b2aa4f13f7ba5098b6b9c0515d21d89da90389dc
DavidKoppula <68698765+DavidKoppula@users.noreply.github.com>
2020-07-26 12:24:23 +0530

    Update Compute.java

--- Compute.java
+++ Compute.java
@@ -1,12 +1,18 @@
 class Compute {
- double calculateCompoundInterest(double Principal,double rate,double time_period)
- {
-	 return (Principal*(Math.pow(1+rate/100,time_period)));
- }
-  float CalculateSimpleInterest(float principal,float rate,float time_period)
-	{
-		return (principal*rate*time_period)/100;
+	double principal, rate_of_interest, timePeriod;
+	
+	Compute(double principal, double rate_of_interest, double timePeriod){
+		this.principal = principal;
+		this.rate_of_interest = rate_of_interest;
+		this.timePeriod = timePeriod;
+	}
+	
+	double calculateCompoundInterest() {
+		return (principal * (Math.pow(1 + rate_of_interest / 100, timePeriod)));
 	}
 
+	double CalculateSimpleInterest() {
+		return (principal * rate_of_interest * timePeriod) / 100;
+	}
 
 }
