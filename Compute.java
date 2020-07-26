fe5d12cbf9a488148f8bb8d0a7294afb400cf680
DavidKoppula <68698765+DavidKoppula@users.noreply.github.com>
2020-07-26 12:02:16 +0530

    Update and rename computations.java to Compute.java

--- Compute.java
+++ Compute.java
@@ -1,6 +1,4 @@
-package com.Interest;
-import java.lang.Math;
-public class Interest {
+class Compute {
  double calculateCompoundInterest(double Principal,double rate,double time_period)
  {
 	 return (Principal*(Math.pow(1+rate/100,time_period)));
