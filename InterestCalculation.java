1458815b94500c8747060125e10103b27bc672dd
DavidKoppula <68698765+DavidKoppula@users.noreply.github.com>
2020-07-26 11:59:28 +0530

    Update InterestCalculation.java

--- InterestCalculation.java
+++ InterestCalculation.java
@@ -1,6 +1,6 @@
 package com.Interest;
 import java.util.*;
-public class CalculatingInterests {
+class CalculatingInterests {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
